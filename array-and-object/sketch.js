7a54c87d394217b8bf550b3aa41b5619575f81db
sadiemcc <sadiekuzyk@gmail.com>
2025-10-10 09:23:40 -0600

    started ride the bus game

--- array-and-object/sketch.js
+++ array-and-object/sketch.js
@@ -1,8 +1,19 @@
-// Card Game
-// Garbage?
-// Rummoli?
-// 
+// Ride the Bus Card Game
+// Sadie McConnell
+// Comp Sci 30
+// 10/10/25
 
+// RULES
+// Round 1 : give one card face down, player guesses if it's red or black
+// Round 2 : player guesses if the next face down card is higher or lower than previous card
+// Round 3 : player guesses if the next face down card is "inside or outside" (in between previous cards or outside)
+// Round 4 : player guesses what suit the last face down card is
+// if player gets it wrong, dealer wins and reset game if some button 
+
+//make an array of each suit and all cards, shuffle array, pop card off when used so no doubles
+//make a spawn card function
+let pickedCard = [];
+let theHearts = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,4 +21,8 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function spawnCard() {
+  
 }
