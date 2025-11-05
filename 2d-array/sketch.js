ade616e354b5a673d1e8f1970dabae399333e829
sadiemcc <sadiekuzyk@gmail.com>
2025-11-05 09:52:59 -0600

    made grids randomize every five seconds

--- 2d-array/sketch.js
+++ 2d-array/sketch.js
@@ -1,36 +1,41 @@
 // 2D Array
-// Simon Says (original or to choose color where color showed)
-// Make grids (easy = 9 spots, medium = 16 spots, hard = 25 spots)
-// Show grid, easy grid, medium grid, hard grid
+// Memorize the grid and redraw it after within 5 seconds
 
-let gameState = "easyMode";
-let theGrid;
+let gameState = "hardMode";
+let theGrid = [[0, 0, 0, 0],
+               [0, 0, 0, 0],
+               [0, 0, 0, 0],
+               [0, 0, 0, 0]];
+const SQUARE_DIMENTIONS = theGrid.length;
 let previousPressed;
-const EASY_DIMENTIONS = 3;
-const MED_DIMENTIONS = 4;
-const HARD_DIMENTIONS = 5;
-
+let lastSwitched = 0;
+let shownDuration = 5000;
+let drawDuration = 8000;
 let cellSize;
+let previousRound = [];
+let state = "drawing";
 
 function setup() {
   createCanvas(windowWidth * 0.9, windowHeight * 0.9);
-  if (width < height && gameState === "easyMode"){
-    cellSize = width/EASY_DIMENTIONS;
+  if (width < height && gameState === "hardMode"){
+    cellSize = width/SQUARE_DIMENTIONS;
   }
-  else if (width > height && gameState === "easyMode"){
-    cellSize = height/EASY_DIMENTIONS;
+  else if (width > height && gameState === "hardMode"){
+    cellSize = height/SQUARE_DIMENTIONS;
   }
-  theGrid = generateGrid(EASY_DIMENTIONS, EASY_DIMENTIONS);
+ 
 }
 
 function draw() {
+  
+  passingTime();
   background("navy");
   showGrid();
 }
 
 function showGrid(){
-  for (let y= 0; y < EASY_DIMENTIONS; y++){
-    for (let x= 0; x < EASY_DIMENTIONS; x++){
+  for (let y= 0; y < SQUARE_DIMENTIONS; y++){
+    for (let x= 0; x < SQUARE_DIMENTIONS; x++){
       if (theGrid[y][x] === 1){
         fill("black");
       }
@@ -58,7 +63,38 @@ function toggleCell(x, y){
   }
 }
 
-function generateGrid(columns, rows){
+function passingTime(){
+  if (millis() > lastSwitched + shownDuration && state === "drawing"){
+    generateRandomGrid();
+    lastSwitched = millis();
+    theGrid = generateRandomGrid(SQUARE_DIMENTIONS, SQUARE_DIMENTIONS);
+    let state = "shown";
+  }
+  else if (millis() > lastSwitched + drawDuration && state === "shown"){
+    generateEmptyGrid();
+    lastSwitched = millis();
+    theGrid = generateEmptyGrid(SQUARE_DIMENTIONS, SQUARE_DIMENTIONS);
+    let state = "drawing";
+  }
+}
+
+function generateRandomGrid(columns, rows){
+  let newGrid = [];
+  for (let y = 0; y < rows; y++){
+    newGrid.push([]);
+    for (let x = 0; x < columns; x++){
+      if (random(100) < 50){
+        newGrid[y].push(1);
+      }
+      else{
+        newGrid[y].push(0);
+      }
+    }
+  }
+  return newGrid;
+}
+
+function generateEmptyGrid(columns, rows){
   let newGrid = [];
   for (let y = 0; y < rows; y++){
     newGrid.push([]);
