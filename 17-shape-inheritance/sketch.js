fcedfec7d7b27fc98a2d1fab2949059ea4c5e7e8
sadiemcc <sadiekuzyk@gmail.com>
2025-11-19 09:29:26 -0600

    shape inheritance oop visual demo

---
+++ 17-shape-inheritance/sketch.js
@@ -0,0 +1,74 @@
+// Shape Inheritance demo
+
+let shapes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  for (let i = 0; i < 20; i++){
+    let choice = random(100);
+    if (choice < 50){
+      let theColor = color(random(255), random(255), random(255));
+      let aShape = new Circle(random(width), random(height), theColor, random(20, 50));
+      shapes.push(aShape);
+    }
+    else {
+      let theColor = color(random(255), random(255), random(255));
+      let aShape = new Square(random(width), random(height), theColor, random(20, 50));
+      shapes.push(aShape);
+    }
+  }
+}
+
+function draw() {
+  background(220);
+
+  for (let shape of shapes){
+    shape.move();
+    shape.display();
+  }
+}
+
+class Shape{
+  constructor(x, y, color){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+  }
+
+  display(){
+    fill(this.color);
+    noStroke();
+  }
+
+  move(){
+    this.x += random(-2, 2);
+    this.y += random(-2, 2);
+  }
+}
+
+class Circle extends Shape {
+  constructor(x, y, color, radius){
+    super(x, y, color);
+    this.radius = radius;
+  }
+
+  display(){
+    // grabs code from parent display
+    super.display();
+    circle(this.x, this.y, this.radius*2);
+  }
+}
+
+class Square extends Shape {
+  constructor(x, y, color, sides){
+    super(x, y, color);
+    this.sides = sides;
+  }
+
+  display(){
+    super.display();
+    rectMode(CENTER);
+    square(this.x, this.y, this.sides);
+  }
+}
